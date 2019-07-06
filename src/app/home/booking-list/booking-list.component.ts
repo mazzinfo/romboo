202c304ac8bbf80e80f3abcd4d37e7027127719d
mazzinfo <mazztechinfo@gmail.com>
2019-07-07 00:44:13 +0530

    booking list selected row

--- src/app/home/booking-list/booking-list.component.ts
+++ src/app/home/booking-list/booking-list.component.ts
@@ -8,14 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingListComponent implements OnInit {
 
+   gridApi:any;
+  gridColumnApi:any;
+
   constructor(public restDataApiService:RestDataApiService) { }
 
   columnDefs = [
-    { displayName: 'Actions', cellTemplate: 
-             '<div class="grid-action-cell">'+
-             '<a (click)="deleteThisRow(row.entity);" >Delete</a></div>'},
-    {headerName: 'Guest Name', field: 'guestName',sortable: true, filter: true },
-    {headerName: 'Company Name', field: 'debtorName',sortable: true, filter: true },
+    {headerName: 'Guest Name', field: 'guestName',sortable: true, filter: true ,checkboxSelection: true ,pinned:"left"},
+    {headerName: 'Company Name', field: 'debtorName',sortable: true, filter: true,pinned:"left" },
     {headerName: 'Phone No', field: 'phone',sortable: true, filter: true },
     {headerName: 'From Date', field: 'fromDate',sortable: true, filter: true},
     {headerName: 'To Date', field: 'toDate',sortable: true, filter: true},
@@ -26,6 +26,32 @@ export class BookingListComponent implements OnInit {
     {headerName: 'Bill Instr', field: 'billinstr',sortable: true, filter: true},
     {headerName: 'Address', field: 'address',sortable: true, filter: true },
 ];
+
+
+onSelectionChanged() {
+  var selectedRows = this.gridApi.getSelectedRows();
+  var selectedRowsString = "";
+  selectedRows.forEach(function(selectedRow, index) {
+    if (index !== 0) {
+      selectedRowsString += ", ";
+    }
+    selectedRowsString += selectedRow.guestName;
+  });
+console.log("selectedRowsString.."+selectedRowsString);
+
+  document.querySelector("#selectedRows").innerHTML = selectedRowsString;
+}
+
+getSelectedBooking(){
+  console.log("selectedRowsString.."+this.gridApi.getSelectedRows()[0].guestName);
+}
+
+
+onGridReady(params) {
+  this.gridApi = params.api;
+  this.gridColumnApi = params.columnApi;
+  
+}
 
   ngOnInit() {
     this.restDataApiService.getBookingList();
--- src/app/shared/services/rest-api.service.ts
+++ src/app/shared/services/rest-api.service.ts
@@ -10,7 +10,7 @@ export class RestApiService {
 
   // apiUrl = 'http://192.168.1.6:8080/roomapi/';
 
-  apiUrl = 'http://192.168.1.3:8090/roomapi/';
+  apiUrl = 'http://192.168.1.2:8090/roomapi/';
 
   constructor(private http: HttpClient) {
 
